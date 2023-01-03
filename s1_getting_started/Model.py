f08329aa9079895d020536e64d049afd8bcca255
OrpheusAthan <90314093+OrpheusAthan@users.noreply.github.com>
2023-01-03 10:31:12 +0100

    Add files via upload

---
+++ s1_getting_started/Model.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+# Build a feed-forward network
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10))
+
+# Define the loss
+criterion = nn.CrossEntropyLoss()
+
+# Get our data
+dataiter = iter(trainloader)
+
+images, labels = next(dataiter)
+
+# Flatten images
+images = images.view(images.shape[0], -1)
+
+# Forward pass, get our logits
+logits = model(images)
+# Calculate the loss with the logits and the labels
+loss = criterion(logits, labels)
+
+print(loss)
